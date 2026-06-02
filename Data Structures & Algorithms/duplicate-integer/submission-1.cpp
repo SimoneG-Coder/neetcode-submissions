9a8fe5abed6fa370abb1851abc3d5c1c666d8abc
SimoneG-Coder <simone22012006@gmail.com>
2026-06-02 16:47:19 +0530

    Update submission-1.cpp

--- Data Structures & Algorithms/duplicate-integer/submission-1.cpp
+++ Data Structures & Algorithms/duplicate-integer/submission-1.cpp
@@ -1,9 +1,12 @@
 class Solution {
 public:
     bool hasDuplicate(vector<int>& nums) {
+        //The unordered set contains the elements iterated so far
         unordered_set<int> seenNum;
         for(int num:nums){
+            //count(key) counts the number of elements but since unordered set only contains unique elements, it will return 0 or 1 here
             if(seenNum.count(num)) return true;
+            //check if the number is there in unordered_set. If there=>duplicate found, otherwise insert that element in the set
             seenNum.insert(num);
         }
         return false;
