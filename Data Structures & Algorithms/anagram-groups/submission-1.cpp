d5a8f0dc6d481f9c61d768b0f85c5390b6812e4e
SimoneG-Coder <simone22012006@gmail.com>
2026-06-02 17:24:07 +0530

    Update submission-1.cpp

--- Data Structures & Algorithms/anagram-groups/submission-1.cpp
+++ Data Structures & Algorithms/anagram-groups/submission-1.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//LOGIC: create an unordered_map that contains a sorted string as the key and the vector of unsorted strings as the values
+//iterate through the given vector and sort the current string. Using the sorted string as a ket, insert the current string along with its anagrams
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         unordered_map<string, vector<string>> anaMap;
         for(string s:strs){
