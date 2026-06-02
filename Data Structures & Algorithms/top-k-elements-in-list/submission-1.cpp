5ff127d01885f74dc519bd37909c8e32ef09232b
SimoneG-Coder <simone22012006@gmail.com>
2026-06-02 17:40:43 +0530

    Update submission-1.cpp

--- Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.cpp
@@ -1,12 +1,16 @@
 class Solution {
 public:
+//LOGIC: use an unordered_map to keep track of frequency of each element in the given array
+//Then use a priority queue to store elements based on their frequency
     vector<int> topKFrequent(vector<int>& nums, int k) {
         unordered_map<int, int> count;
         for(int num: nums){
             count[num]++;
         }
+        //priority queue<type, container<type>, greater/less<type>> greater for minHeap(smallest on top)
         priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
         for(auto &val: count){
+            //first element in priority queue gets higher priority, we want queue based on frequency
             minHeap.push({val.second, val.first});
             if(minHeap.size()>k)minHeap.pop();
         }
