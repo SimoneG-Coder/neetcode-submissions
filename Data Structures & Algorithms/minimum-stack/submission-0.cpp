b2a84b9fbed668ec9a764c38a83b6402ebf559be
SimoneG-Coder <simone22012006@gmail.com>
2026-06-02 22:47:51 +0530

    Add: minimum-stack - submission-0

---
+++ Data Structures & Algorithms/minimum-stack/submission-0.cpp
@@ -0,0 +1,28 @@
+class MinStack {
+public:
+    MinStack() {
+        
+    }
+    
+    void push(int val) {
+        st.push(val);
+        if(minSt.empty() || minSt.top()>val) minSt.push(val);
+        else minSt.push(minSt.top());
+    }
+    
+    void pop() {
+        st.pop();
+        minSt.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minSt.top();
+    }
+private:
+    stack<int> st;
+    stack<int> minSt;
+};
