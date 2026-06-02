e51cd1b8d05ce3e0c7425edb307d2a7ea321851f
SimoneG-Coder <simone22012006@gmail.com>
2026-06-02 18:13:01 +0530

    Add: string-encode-and-decode - submission-1

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-1.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+
+    string encode(vector<string>& strs) {
+        string encoded;
+        for(string str: strs){
+            encoded += to_string(str.length()) + '#' + str;
+        }
+        return encoded;
+    }
+
+    vector<string> decode(string s) {
+        vector<string> decoded;
+            int i=0; 
+            while(i<s.length()){
+                int j=i;
+                while(s[j]!='#'){
+                    j++;
+                }
+                int length = stoi(s.substr(i, j-i));
+                i = j+1;
+                decoded.push_back(s.substr(i, length));
+                i += length;
+            }
+            return decoded;
+    }
+};
