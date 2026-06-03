2b791c98120686482990e0e88ee56aed1120099c
SimoneG-Coder <simone22012006@gmail.com>
2026-06-03 17:05:49 +0530

    Update submission-0.cpp

--- Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
+++ Data Structures & Algorithms/evaluate-reverse-polish-notation/submission-0.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//LOGIC: push integers in stack till you get an operator
+//the element on top of stack should be element on right side of the operation
     int evalRPN(vector<string>& tokens) {
         stack<int> st;
         for(auto& t:tokens){
@@ -13,7 +15,9 @@ public:
                 else if(t =="-") st.push(a-b);
                 else if(t =="*") st.push(a*b);
                 else{
+                    //use static_cast<type>(variable) for typecasting 
                     double division = static_cast<double>(a)/b;
+                    //since we want the output for division to be close to 0, use floor division for positive results and ceil division for negative ones
                     st.push(division<0? static_cast<int>(ceil(division)): static_cast<int>(floor(division)));
                 }
                 
