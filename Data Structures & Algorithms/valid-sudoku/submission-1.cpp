b8cf3444db3fb0a3d71f3d8e626863424ab03b05
SimoneG-Coder <simone22012006@gmail.com>
2026-06-03 17:44:20 +0530

    Update submission-1.cpp

--- Data Structures & Algorithms/valid-sudoku/submission-1.cpp
+++ Data Structures & Algorithms/valid-sudoku/submission-1.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+//LOGIC: use coolean arrays to check if a number is a duplicate or not
+//[10] to make indexing easier as each number will be in its corresponding clock
     bool isValidSudoku(vector<vector<char>>& board) {
         bool rows[9][10] = {false};
         bool cols[9][10] = {false};
@@ -7,7 +9,9 @@ public:
         for(int r=0; r<9; r++){
             for(int c=0; c<9; c++){
                 if(board[r][c] == '.') continue;
+                //use -'0' to convert string into integer
                 int num = board[r][c] - '0';
+                //use this formula to reduce coordinates like (2,4) into its repective box number
                 int boxIndex = (r/3) * 3 + (c/3);
                 if(rows[r][num] || cols[c][num] || boxes[boxIndex][num]) return false;
                 rows[r][num] = true;
