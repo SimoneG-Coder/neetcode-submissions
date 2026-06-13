a92c63048ab66da99988ec949664d0d7670b2f33
SimoneG-Coder <simone22012006@gmail.com>
2026-06-13 16:43:10 +0530

    Add: find-duplicate-integer - submission-0

---
+++ Data Structures & Algorithms/find-duplicate-integer/submission-0.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        int hare = nums[0];
+        int tortoise = nums[0];
+         
+         do{
+            hare = nums[nums[hare]];
+            tortoise = nums[tortoise];
+         } while(tortoise != hare);
+
+         tortoise = nums[0];
+
+         while(hare != tortoise){
+            hare = nums[hare];
+            tortoise = nums[tortoise];
+         }
+         return hare;
+    }
+};
