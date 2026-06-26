288e1ee557328137c36f97ede887b29d3f30821e
SimoneG-Coder <simone22012006@gmail.com>
2026-06-26 20:35:58 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit = 0;
+        int minPrice = INT_MAX;
+        for(int price: prices){
+            if(price < minPrice) minPrice = price;
+            else if(price - minPrice > maxProfit) maxProfit = price - minPrice;
+        }
+        return maxProfit;
+    }
+};
