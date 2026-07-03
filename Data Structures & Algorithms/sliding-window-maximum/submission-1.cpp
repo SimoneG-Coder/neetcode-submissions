e2e267e4e2814e5140855b8b84b53e0545b2cd8c
SimoneG-Coder <simone22012006@gmail.com>
2026-07-03 21:29:07 +0530

    Update submission-1.cpp

--- Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
+++ Data Structures & Algorithms/sliding-window-maximum/submission-1.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
+//LOGIC: use a deque  to store the indices of elements(in descending order) where the elements of indices of deque are removed from the back till they are smaller han the current element
     vector<int> maxSlidingWindow(vector<int>& nums, int k) {
       vector<int> result;
       deque<int> dq;
       for(int right=0; right<nums.size(); right++){
-        if(!dq.empty()&& dq.front()<right-k+1) dq.pop_front();
-        while(!dq.empty()&&nums[dq.back()]<=nums[right]) dq.pop_back();
-        dq.push_back(right);
-        if(right>=k-1) result.push_back(nums[dq.front()]);
+        if(!dq.empty()&& dq.front()<right-k+1) dq.pop_front();    //If the first element is out of the window, pop it
+        while(!dq.empty()&&nums[dq.back()]<=nums[right]) dq.pop_back();    //pop the elements from the back till they are smaller than the current element
+        dq.push_back(right);    //push back the current element
+        if(right>=k-1) result.push_back(nums[dq.front()]);    //push the max element(at the front) if the window size is equal to or greater than the given size
       }  
       return result;
     }
